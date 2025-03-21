2e2768475837e3a53ee2bcb36081507416b013f7
Rider Mamani Paco <rider.mamani@ucb.edu.bo>
2025-03-21 00:02:29 -0400

    agregue configuración inicial con variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('APP_PORT');
+
+  await app.listen(port ?? 5000, '0.0.0.0').then(async () => {
+    logger.log(`Server running on ${await app.getUrl()}`);
+    logger.log(`Server running on ${(await app.getUrl()) + '/api'}`);
+  });
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error(err);
+});
