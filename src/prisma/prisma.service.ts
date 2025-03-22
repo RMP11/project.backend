6dc474b6782ee9c64e2aca0b5ee1c8f712797dea
Rider Mamani Paco <rider.mamani@ucb.edu.bo>
2025-03-22 18:10:20 -0400

    agregue modulo de prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,22 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   private logger = new Logger('PrismaService');
   async onModuleInit() {
     await this.$connect(); // Conectar automáticamente con la base de datos
     this.logger.log('Prisma Service Initialized');
   }
 
-  // Método que se ejecuta cuando el módulo se destruye
   async onModuleDestroy() {
     await this.$disconnect(); // Desconectar la base de datos
   }
